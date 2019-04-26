7ec56dd768a841db6cde09906ce94a2f3798836c
Reind Dooyeweerd <reind@mini-van-reind.home>
2019-04-26 13:22:43 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import 'popper.js';
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/popover';
+import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/tooltip';
+import 'bootstrap/js/dist/util';
+import "./sass/style.scss";
+
+"use strict";
+
+(function($) {
+	$(function() {
+		console.log("Document Ready");
+	});
+})(jQuery);
